82db7391223554dddc2e4ed0b8edbc4e60855fe4
BernhardBaumrock <office@baumrock.com>
2020-04-25 13:38:19 +0200

    add textformatter + readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TextformatterRemoveEmptyParagraphs
+
 ProcessWire Textformatter to remove empty paragraphs from CKE fields
+
+Removes empty paragraphs from a html string. A paragraph is considered to be empty if it only contains one or more of the following:
+
+* regular whitespace
+* UTF-8 encoded whitespace
+* all variants of `<br/>`
--- TextformatterRemoveEmptyParagraphs.module.php
+++ TextformatterRemoveEmptyParagraphs.module.php
@@ -1,1 +1,21 @@
 <?php namespace ProcessWire;
+/**
+ * Textformatter to remove empty paragraphs from CKE fields
+ *
+ * @author Bernhard Baumrock, 25.04.2020
+ * @license Licensed under MIT
+ * @link https://www.baumrock.com
+ */
+class TextformatterRemoveEmptyParagraphs extends Textformatter {
+  public static function getModuleInfo() {
+    return [
+      'title' => 'RemoveEmptyParagraphs',
+      'version' => '1.0.0',
+      'summary' => 'Textformatter to remove empty paragraphs from CKE fields',
+    ]; 
+  }
+
+  public function format(&$str) {
+    $str = preg_replace('!<p[^>]*>([\xc2\xa0]*|\s| |<\/?\s?br[^>]*\/?>)*<\/?p>!', "", $str);
+  }
+}
